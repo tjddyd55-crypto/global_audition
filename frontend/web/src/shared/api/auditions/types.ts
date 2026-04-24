271dbc6ad1e5fc8e0162427a6d0348c6a26924aa
tjddyd55-crypto <tjddyd55@gmail.com>
2026-04-24 17:20:34 +0900

    refactor(frontend): add audition api type boundary

---
+++ frontend/web/src/shared/api/auditions/types.ts
@@ -0,0 +1,16 @@
+export type {
+  AgencyBoardStatus,
+  ApplicationAgencyDetail,
+  ManageApplicantItem,
+  ManageApplicationStats,
+  ManageApplicationsPayload,
+  ManageAuditionHeader,
+  ManageListFilters,
+  ManageRoundCount,
+  PublicVoteItem,
+  PublicVotesPagePayload,
+  RankingItem,
+  VotePageCategory,
+} from '../auditions'
+
+export type { AuditionDto, AuditionImages, CreateAuditionPayload } from '../../types/audition'
