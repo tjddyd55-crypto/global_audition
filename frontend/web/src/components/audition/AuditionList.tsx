ff1d4e3ee46dd6ee9b56fe8dc079443b8744d2d1
tjddyd55-crypto <tjddyd55@gmail.com>
2026-04-26 19:51:15 +0900

    refactor(frontend): split audition list query and content

--- frontend/web/src/components/audition/AuditionList.tsx
+++ frontend/web/src/components/audition/AuditionList.tsx
@@ -1,42 +1,10 @@
 ﻿'use client'
 
-import { useQuery } from '@tanstack/react-query'
-import { auditionApi } from '@/shared/api/auditions'
-import type { AuditionDto } from '@/shared/types/audition'
-import AuditionCard from './AuditionCard'
-import { SkeletonAuditionCard } from '../ui/SkeletonCard'
-import EmptyState from '../ui/EmptyState'
-import ErrorMessage from '../common/ErrorMessage'
+import { useAuditionsListQuery } from './useAuditionsListQuery'
+import AuditionListContent from './AuditionListContent'
 
 export default function AuditionList() {
-  const { data, isLoading, error } = useQuery({
-    queryKey: ['auditions'],
-    queryFn: () => auditionApi.listOpen(),
-  })
+  const { data, isLoading, error } = useAuditionsListQuery()
 
-  if (isLoading) {
-    return (
-      <div className="flex w-full flex-col lg:gap-3">
-        {[1, 2, 3, 4, 5, 6].map((i) => (
-          <SkeletonAuditionCard key={i} />
-        ))}
-      </div>
-    )
-  }
-
-  if (error) {
-    return <ErrorMessage message="오디션 목록을 불러오는데 실패했습니다." />
-  }
-
-  if (!data || data.length === 0) {
-    return <EmptyState message="등록된 오디션이 없습니다" />
-  }
-
-  return (
-    <div className="flex w-full flex-col lg:gap-3">
-      {data.map((audition: AuditionDto) => (
-        <AuditionCard key={audition?.id ?? ''} audition={audition} />
-      ))}
-    </div>
-  )
+  return <AuditionListContent auditions={data} isLoading={isLoading} error={error} />
 }
