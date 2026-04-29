165dc08612d72f5a2a752255f0503805a0ca0cc7
tjddyd55-crypto <tjddyd55@gmail.com>
2026-04-29 21:26:53 +0900

    refactor(frontend): export applicant detail components

---
+++ frontend/web/src/components/audition/manage/detail/index.ts
@@ -0,0 +1,3 @@
+﻿export { default as ApplicantDetailActionBar } from './ApplicantDetailActionBar'
+export { default as ApplicantDetailBasicInfo } from './ApplicantDetailBasicInfo'
+export * from './applicantDetailLabels'
